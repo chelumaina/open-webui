ebd3083eb5a6493a2fd5c0ef51085222d4a598c9
Chelule Maina <chelumaina@gmail.com>
2025-10-30 14:59:52 +0300

    Subscription and daily limit fix

--- src/lib/components/payment/PaystackCheckout.svelte
+++ src/lib/components/payment/PaystackCheckout.svelte
@@ -201,7 +201,7 @@
 				d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z"
 			></path>
 		</svg>
-		{$i18n.t('Subscribe to')} {plan.name}
+		{$i18n.t('Subscribe to')} {plan.name} - {formatPrice(plan.amount, plan.currency)} {disabled} {disabled? `(${ $i18n.t('Unavailable') })` : ''}
 	{/if}
 </button>
 
